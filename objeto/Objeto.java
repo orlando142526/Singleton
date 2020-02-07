d6d010a7b681376bf8233e3c640011f3e140009d
orlando142526 <56253433+orlando142526@users.noreply.github.com>
2020-02-07 13:26:02 +0100

    Update Objeto.java

--- objeto/Objeto.java
+++ objeto/Objeto.java
@@ -8,8 +8,8 @@ public class Objeto {
 
   
   public static void main(String[] args) {
-       Persona obx1= Persona.getInstance("Pepe", 34, "Guerrero");
-       Persona obx2=Persona.getInstance("Roberto",36,"DosSantos");
+       Persona obx1= Persona.getInstance("Pepe", 12, "Hernandez");
+       Persona obx2=Persona.getInstance("Luis",96,"Sanchez");
         System.out.println(obx1.getNombre());
         System.out.println(obx2.getNombre());
     }
